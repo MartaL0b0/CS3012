aa759c5bdfe35c3ea448c3dc5255cc4894d55fd7
DESKTOP-4TPE2CT\MartaL0b0 <martaldp@gmail.com>
2018-09-24 21:59:13 +0100

    Add implementation. First test not passing.

--- GraphsCS3012/GraphCS3012Test.py
+++ GraphsCS3012/GraphCS3012Test.py
@@ -1,12 +1,12 @@
 import unittest 
 import GraphCS3012
 
-class Test_TestAddTwo(unittest.TestCase):
-    def test_addTwo(self):
-        self.assertEquals(GraphCS3012.addTwo(5), 7)
 
-    def test_addTowNeg(self):
-        self.assertEquals(GraphCS3012.addTwo(-1), 1)
+class Test_TestFindLCA(unittest.TestCase):
+    def test_findLCA(self):
+        root = GraphCS3012.Node(1)
+        self.assertEquals(GraphCS3012.findLCA(root, 5,6), 1)
+
 
 if __name__ == '__main__':
     unittest.main()
