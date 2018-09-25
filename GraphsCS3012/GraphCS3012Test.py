6f65b7203f47fcb48439f0807a7f82e4c5130381
DESKTOP-4TPE2CT\MartaL0b0 <martaldp@gmail.com>
2018-09-25 22:07:04 +0100

    New try for test. Still not working

--- GraphsCS3012/GraphCS3012Test.py
+++ GraphsCS3012/GraphCS3012Test.py
@@ -3,8 +3,15 @@ import GraphCS3012
 
 
 class Test_TestFindLCA(unittest.TestCase):
+
     def test_findLCA(self):
         root = GraphCS3012.Node(1)
+        root.left = GraphCS3012.Node(2)
+        root.right = GraphCS3012.Node(3)
+        root.left.left = GraphCS3012.Node(4)
+        root.left.right = GraphCS3012.Node(5)
+        root.right.left = GraphCS3012.Node(6)
+        root.right.right = GraphCS3012.Node(7)
         self.assertEquals(GraphCS3012.findLCA(root, 5,6), 1)
 
 
