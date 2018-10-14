1f4ca66a32e9f61ad270ff67314308e82b66b069
DESKTOP-4TPE2CT\MartaL0b0 <martaldp@gmail.com>
2018-10-14 19:45:26 +0100

    working code with libraries

--- GraphsCS3012/networkxTry.py
+++ GraphsCS3012/networkxTry.py
@@ -1,12 +1,12 @@
-from networkx import networkx as nx
+import networkx as nx
 
 G = nx.DiGraph()  # Directed graph
-G.add_nodes_from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
-G.add_edges_from([(2, 1), (3, 1), (4, 1), (5, 2), (6, 2), (7, 3), (8, 3), (9, 3),
-                  (10, 4), (10, 12), (14, 9), (15, 8), (12, 11), (13, 11), (14, 12), (15, 13)])
+G.add_nodes_from([1, 2, 3, 4, 5, 6, 7])
+edges = [(1, 2), (2, 3), (3, 4), (4, 7), (5, 6), (6, 7), (2, 5)]
+G.add_edges_from([(e[1], e[0]) for e in edges])
+print(nx.algorithms.lowest_common_ancestor(G, 1, 5))
 
-if __name__ == "__main__":
-    print(nx.algorithms.lowest_common_ancestor(G, 1, 11))
+
 
 
 
