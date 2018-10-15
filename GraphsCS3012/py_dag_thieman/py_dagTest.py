ee971de3b003b24e01d865b61f270383a9001263
DESKTOP-4TPE2CT\MartaL0b0 <martaldp@gmail.com>
2018-10-15 18:43:27 +0100

    More add_element and delete_element tests

--- GraphsCS3012/py_dag_thieman/py_dagTest.py
+++ GraphsCS3012/py_dag_thieman/py_dagTest.py
@@ -20,11 +20,44 @@ class TestPyDag(unittest.TestCase):
         self.dag.delete_node(1)
         self.assertTrue(self.dag.graph == {})
 
+    def testDeleteNonExistingNode(self):
+        self.dag.add_node(1)
+        with self.assertRaises(KeyError):
+            self.dag.delete_node(2)
+
+    def testDeleteNodeIfExists(self):
+        self.dag.add_node(1)
+        self.assertTrue(self.dag.graph == {1: set()})
+        self.dag.delete_node_if_exists(2)
+        self.assertTrue(self.dag.graph == {1: set()})
+        #nothing should happen: pass can not be tested explicitly
+
     def testAddEdge(self):
         self.dag.add_node(1)
         self.dag.add_node(2)
         self.dag.add_edge(1,2)
         self.assertTrue(self.dag.graph == {1: set([2]), 2: set()})
+
+    def testAddEdgeNonExistingNodes(self):
+        self.dag.add_node(1)
+        self.dag.add_node(2)
+        with self.assertRaises(KeyError): 
+            self.dag.add_edge(1, 3)
+
+        
+            
+
+
+    def testDeleteEdge(self):
+        self.dag.add_node(1)
+        self.dag.add_node(2)
+        self.dag.add_edge(1, 2)
+        self.dag.delete_edge(1, 2)
+        self.assertTrue(self.dag.graph == {1: set(), 2: set()})
+
+   # def testDeleteNonExistingEdge(self):
+
+
 
 if __name__ == '__main__':
     unittest.main()
