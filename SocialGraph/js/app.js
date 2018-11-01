77681488cb1c609c953e12325475d7d425e1b3ff
DESKTOP-4TPE2CT\MartaL0b0 <martaldp@gmail.com>
2018-11-01 22:13:12 +0000

    Make call to API and prove it works

--- SocialGraph/js/app.js
+++ SocialGraph/js/app.js
@@ -4,3 +4,24 @@ const nameContainer = document.querySelector(".main__profile-name");
 const unContainer = document.querySelector(".main__profile-username");
 const reposContainer = document.querySelector(".main__profile-repos");
 const urlContainer = document.querySelector(".main__profile-url");
+
+const client_id = 'Iv1.998edc0aa181fcce';
+const client_secret = 'cc10fc862c91e2ea6cfbdaf26410f2c65e38ac19';
+
+const fetchUsers = async (user) => {
+    const api_call = await fetch(`https://api.github.com/users/${user}?client_id=${client_id}&client_secret=${client_secret}`);
+
+    const data = await api_call.json();
+    return { data }
+};
+
+const showData = () => {
+    console.log(`Querying user ${inputValue.value}`);
+    fetchUsers(inputValue.value).then((res) => {
+        console.log(res);
+    })
+};
+
+searchButton.addEventListener("click", () => {
+    showData();
+})
