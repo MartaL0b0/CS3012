2c77c263486dff1a789bbec61dabd12da0f71be5
DESKTOP-4TPE2CT\MartaL0b0 <martaldp@gmail.com>
2018-11-04 11:32:24 +0000

    change way of calling api. server needed

--- SocialGraph/js/app.js
+++ SocialGraph/js/app.js
@@ -1,3 +1,10 @@
+var github = require(octonode);
+
+var client = github.client({
+    id: 'Iv1.998edc0aa181fcce',
+    secret: 'cc10fc862c91e2ea6cfbdaf26410f2c65e38ac19'
+});
+
 const inputValue = document.querySelector("#search");
 const searchButton = document.querySelector(".searchButton");
 const nameContainer = document.querySelector(".main__profile-name");
@@ -5,13 +12,16 @@ const unContainer = document.querySelector(".main__profile-username");
 const reposContainer = document.querySelector(".main__profile-repos");
 const urlContainer = document.querySelector(".main__profile-url");
 
-const client_id = 'Iv1.998edc0aa181fcce';
-const client_secret = 'cc10fc862c91e2ea6cfbdaf26410f2c65e38ac19';
+/* const client_id = 'Iv1.998edc0aa181fcce';
+const client_secret = 'cc10fc862c91e2ea6cfbdaf26410f2c65e38ac19'; */
 
 const fetchUsers = async (user) => {
-    const api_call = await fetch(`https://api.github.com/users/${user}?client_id=${client_id}&client_secret=${client_secret}`);
-
-    const data = await api_call.json();
+   // const api_call = await fetch(`https://api.github.com/users/${user}?client_id=${client_id}&client_secret=${client_secret}`);
+    
+    var data;
+    client.get('/user', {}, function (err, status, body, headers) {
+        data = body; //json object
+    });
     return { data }
 };
 
