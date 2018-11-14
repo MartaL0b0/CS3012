ab104fbe8990c54abc9cd3355d047a55ff8abffe
DESKTOP-4TPE2CT\MartaL0b0 <martaldp@gmail.com>
2018-11-14 11:16:00 +0000

    add functionality to get commit stats data

--- SocialGraph/public/js/app.js
+++ SocialGraph/public/js/app.js
@@ -1,6 +1,9 @@
 const userValue = document.querySelector("#username");
 const passwordValue = document.querySelector("#password");
-const searchButton = document.querySelector(".searchButton");
+const repoUser = document.querySelector("#repoUser");
+const repoName = document.querySelector("#repoName");
+const searchButton = document.querySelector("#searchButton");
+const statsButton = document.querySelector("#statsButton");
 const nameContainer = document.querySelector(".main__profile-name");
 const unContainer = document.querySelector(".main__profile-username");
 const reposContainer = document.querySelector(".main__profile-repos");
@@ -38,6 +41,40 @@ const showData = () => {
     })
 };
 
+const showGithubData = () => {
+    console.log(`Querying user  ${repoUser.value} and repo ${repoName.value}`);
+    fetchPunchCard(repoUser.value, repoName.value).then((res) => {
+        console.log(res);
+
+        /* nameContainer.innerHTML = `Name: <span class="main__profile-value">${res.data.name}</span>`;
+        unContainer.innerHTML = `Username: <span class="main__profile-value">${res.data.login}</span>`;
+
+        reposContainer.innerHTML = `Repositories: <span class="main__profile-value">${res.data.public_repos}</span>`;
+
+        urlContainer.innerHTML = `URL: <span class="main__profile-value">${res.data.html_url}</span>`;
+
+        avatarContainer.innerHTML = `<img src="${res.data.avatar_url}" alt="Smiley face" height="150">`;
+ */    
+    })
+};
+
+
+const fetchPunchCard = async (username, repository) => {
+   /*  GET / repos /: owner /: repo / stats / punch_card */
+    const url = `https://api.github.com/repos/${username}/${repository}/stats/punch_card`;
+    const auth = btoa(username + ":" + password);
+   
+
+    const api_call = await fetch(url);
+
+    const data = await api_call.json();
+    return { data }
+};
+
 searchButton.addEventListener("click", () => {
     showData();
+})
+
+statsButton.addEventListener("click", () => {
+    showGithubData();
 })
