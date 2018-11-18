b8d96823a9e76169ba43bf5a9ff2a3aa65f1811a
DESKTOP-4TPE2CT\MartaL0b0 <martaldp@gmail.com>
2018-11-18 17:45:30 +0000

    request punchcard for each repo when clicking

--- SocialGraph/public/js/app.js
+++ SocialGraph/public/js/app.js
@@ -2,11 +2,14 @@ const userValue = document.querySelector("#username");
 const passwordValue = document.querySelector("#password");
 const searchButton = document.querySelector("#searchButton");
 const reposList = document.querySelector(".list-group");
+let userName;
+let auth;
+let userParams;
 
 const fetchRepositories = async (username, password) => {
-    const url = "https://api.github.com/user/repos"; //change this with reposGET /users/:username/repos
-    const auth = btoa(username + ":" + password);
-    const userParams = {
+    const url = "https://api.github.com/user/repos"; 
+    auth = btoa(username + ":" + password);
+    userParams = {
         headers: {
             'Authorization': 'Basic ' + auth
         }
@@ -20,19 +23,53 @@ const fetchRepositories = async (username, password) => {
 
 const showData = () => {
     console.log(`Querying user ${userValue.value}`);
+    userName = userValue.value;
     fetchRepositories(userValue.value, passwordValue.value).then((response) => {
         console.log(response);
         for (var i in response.data){
-            reposList.innerHTML += `<a href="${response.data[i].html_url}" class="list-group-item list-group-item-action" id="${response.data[i].owner.login}_${response.data[i].name}">
-                                        ${response.data[i].name}</a>`; 
-            if (response.data[i].owner.login.toLowerCase() !== userValue.value.toLowerCase()){
-                document.querySelector(`#${response.data[i].owner.login}_${response.data[i].name}`).innerHTML += ` (Owned by ${response.data[i].owner.login})`; 
+            let repoOwner = response.data[i].owner.login;
+            let repoName = response.data[i].name;
+            reposList.innerHTML += `<a href="#" 
+                                    class="list-group-item list-group-item-action" 
+                                    id="${repoName}">
+                                    ${repoName}
+                                    </a>`; 
+            if (repoOwner.toLowerCase() !== userName.toLowerCase()){
+                document.querySelector(`#${repoName}`).innerHTML += ` (Owned by ${repoOwner})`; 
             }
          
         }
+        var elementsArray = document.querySelectorAll(".list-group-item-action");
+        console.log(elementsArray);
+
+        elementsArray.forEach(function (elem) {
+            elem.addEventListener("click", function () {
+                showRepoStats(elem.id);
+            });
+        });
        
     })
 };
+
+const fetchRepositoryPunchCard = async (username, repository) => {
+    
+    //GET /repos/:owner/:repo/stats/punch_card
+    const url = `https://api.github.com/repos/${username}/${repository}/stats/punch_card`;
+
+    const api_call = await fetch(url);
+
+    const data = await api_call.json();
+    return { data }
+};
+
+const showRepoStats = (repository) => {
+    console.log(`Querying user ${userName} and repo ${repository}`);
+    fetchRepositoryPunchCard(userName, repository).then((response) => {
+        console.log(response);
+    })
+};
+
+
 
 searchButton.addEventListener("click", () => {
     showData();
