5ce10b22c48bce0b1b71027c300b4cafdaee1b54
DESKTOP-4TPE2CT\MartaL0b0 <martaldp@gmail.com>
2018-11-25 19:30:31 +0000

    change column definition to row definition

--- SocialGraph/public/js/barChart.js
+++ SocialGraph/public/js/barChart.js
@@ -26,7 +26,7 @@ const showRepoStats = (repoOwner, repoName) => {
         chart = c3.generate({
             bindto: '#chart',
             data: {
-                columns: transformData(response.data)
+                rows: transformData(response.data)
             }
         });
     })
@@ -38,7 +38,11 @@ const transformData = (response) => {
             ['data1', 30, 200, 100, 400, 150, 250],
             ['data2', 50, 20, 10, 40, 15, 25]
         ]*/
-
+    transformedData = 
+        [
+            ['commits per hour'],
+            [90], [120], [300]
+        ];
     
     return transformedData;
 };
